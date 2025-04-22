1a3c3077351c30ca655fbe52d6ecd0705eac0bbd
PiyushGaywal <142382705+PiyushGaywal@users.noreply.github.com>
2025-04-22 10:37:50 +0530

    Updated Port and added .env

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db=require('./utils/db')
 const dsb=require('./routes/dashboard')
 const session=require('express-session')
 app.set('view engine', 'ejs');
+require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,6 +28,8 @@ app.use(session({
 app.use(authroutes);
 app.use(dsb)
 
-app.listen(8000, () => {
-  console.log('Server Run Success');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
